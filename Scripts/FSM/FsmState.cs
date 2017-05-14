1aef9ac77087a8f3553226b7c76a7585dad543d1
unknown <sidney9111@gmail.com>
2017-05-15 02:39:32 +0800

    add fsm state.cs

---
+++ Scripts/FSM/FsmState.cs
@@ -0,0 +1,18 @@
+﻿namespace BlGame.FSM
+{
+	public enum FsmState 
+	{
+		FSM_STATE_FREE,
+		FSM_STATE_RUN,
+        FSM_STATE_SING,
+		FSM_STATE_RELEASE,
+        FSM_STATE_LEADING,
+        FSM_STATE_LASTING,
+		FSM_STATE_DEAD,
+        FSM_STATE_ADMOVE,
+        FSM_STATE_FORCEMOVE,
+        FSM_STATE_RELIVE,
+        FSM_STATE_IDLE,
+		FSM_STATE_LOCALMOVE,
+	}
+}
